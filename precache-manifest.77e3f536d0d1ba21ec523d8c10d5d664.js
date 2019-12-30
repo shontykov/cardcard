adb1f5053debb0ae3a8af7d04778f8ac120001ec
shontykov <shontykov.kz@gmail.com>
2019-12-30 10:27:11 +0600

    Updates

---
+++ precache-manifest.77e3f536d0d1ba21ec523d8c10d5d664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfcc32a10ba773785d3a3c360bfbddf0",
+    "url": "/app-type/index.html"
+  },
+  {
+    "revision": "5dd9dc7ff7fd838826f7",
+    "url": "/app-type/static/css/main.e05f7078.chunk.css"
+  },
+  {
+    "revision": "1d664d43cdcfb577ba18",
+    "url": "/app-type/static/js/2.289a722b.chunk.js"
+  },
+  {
+    "revision": "37ef80829e57f4a0c2aa4bfe70fe2cf3",
+    "url": "/app-type/static/js/2.289a722b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5dd9dc7ff7fd838826f7",
+    "url": "/app-type/static/js/main.c132d87c.chunk.js"
+  },
+  {
+    "revision": "89d96acc3111fcf67149",
+    "url": "/app-type/static/js/runtime-main.1fe2677b.js"
+  }
+]);
