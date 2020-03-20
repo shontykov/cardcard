22c724760028b328838b4b97fed109dcdc401d93
thepromind <thepromind@gmail.com>
2020-03-21 00:00:09 +0600

    Add New Banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,6 +3,7 @@ import { Grid, Typography, IconButton } from "@material-ui/core";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import { paddingDownSm, rootSmXl } from "./helper/DefaultStyle";
 import CloseIcon from '@material-ui/icons/Close';
+import { useTranslation } from "react-i18next";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -14,8 +15,9 @@ const useStyles = makeStyles((theme: Theme) =>
                 width: '100%',
                 cursor: 'pointer',
                 marginBottom: 16,
+                marginRight: 16,
                 '& img': {
-                    width: '100%',
+                    width: '300px',
                 }
             },
             mainTitle: {
@@ -26,7 +28,10 @@ const useStyles = makeStyles((theme: Theme) =>
                 marginBottom: 24
             },
             bannerMain: {
-                justifyContent: 'space-between'
+                flexWrap: 'nowrap',
+                overflowX: 'auto',
+                justifyContent: 'space-between',
+                width: '100%'
             },
             modalClose: {
                 position: 'relative',
@@ -151,7 +156,7 @@ const useStyles = makeStyles((theme: Theme) =>
         [theme.breakpoints.between("sm", "xl")]: {
             ...rootSmXl,
             fixedRoot: {
-                width: '45%',
+                width: '32%',
                 cursor: 'pointer',
                 '& img': {
                     width: '100%',
@@ -165,7 +170,8 @@ const useStyles = makeStyles((theme: Theme) =>
                 marginBottom: 24
             },
             bannerMain: {
-                justifyContent: 'space-between'
+                justifyContent: 'space-between',
+                flexWrap: 'nowrap'
             },
             modalClose: {
                 position: 'relative',
@@ -292,16 +298,18 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const Banner = () => {
     const classes = useStyles({});
+    const { t } = useTranslation();
     const [open, setOpen] = React.useState(false)
     const [open2, setOpen2] = React.useState(false)
+    const [open3, setOpen3] = React.useState(false)
     const [closed, setClosed] = React.useState(false)
 
     return (
         <Grid container className={classes.root}>
             <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
                 <Typography className={classes.mainTitle}>
-                    Новости и акции
-             </Typography>
+                    {t("banners.mainTitle")}
+                </Typography>
             </Grid>
             <Grid container className={classes.bannerMain}>
                 <Grid className={classes.fixedRoot}>
@@ -320,6 +328,14 @@ const Banner = () => {
 
                 </Grid>
 
+                <Grid className={classes.fixedRoot}>
+
+                    <img src="bannerMain-3.svg" onClick={() => {
+                        setOpen3(true)
+                    }} />
+
+                </Grid>
+
                 <Grid className={`${classes.fixedModal} ${open ? classes.open : ''}`}>
 
 
@@ -332,9 +348,9 @@ const Banner = () => {
                             </div>
                             <Grid className={classes.modalTitle}>
                                 <Typography className={classes.chooseTitle}>
-                                    <span>Выбери категорию Рахмет в приложении StarBanking</span>
-                                    <span>Привяжи #картакарта в приложении Рахмет</span>
-                                    <span>Покупай и получай 5% кэшбэк по #картакарта дополнительно к кэшбэку от Рахмет</span>
+                                    <span>{t("bannersSecond.desc")}</span>
+                                    <span>{t("bannersSecond.desc-2")}</span>
+                                    <span>{t("bannersSecond.desc-3")}</span>
                                 </Typography>
                             </Grid>
 
@@ -353,42 +369,52 @@ const Banner = () => {
                         <IconButton className={classes.fixedBtnModal} onClick={() => setOpen2(false)}><CloseIcon /></IconButton>
                         <Grid className={classes.modalMain}>
                             <Grid className={classes.modalTitle}>
-                                <h3>#картакарта на 1 000 000</h3>
-                                <h4>Срок проведения акции</h4>
-                                <p>С 01.03.2020 г. по 31.03.2020 г.</p>
-                                <h4>Участники акции</h4>
-                                <p>Клиенты, оформившие #картакарта в период Акции и совершившие покупку(-и) в POS/Internet на общую сумму не менее 10 000 тенге в течение 3-х календарных дней с даты получения карты</p>
+                                <h3>{t("bannersFirst.mainTitle")}</h3>
+                                <h4>{t("bannersFirst.text-main")}</h4>
+                                <p>{t("bannersFirst.text-desc")}</p>
+                                <h4>{t("bannersFirst.text-main-1")}</h4>
+                                <p>{t("bannersFirst.text-desc-1")}</p>
 
-                                <h4>География проведения</h4>
-                                <p>Филиальная сеть Банка ЦентрКредит</p>
+                                <h4>{t("bannersFirst.text-main-2")}</h4>
+                                <p>{t("bannersFirst.text-desc-2")}</p>
 
-                                <h4>Продукты</h4>
-                                <p>«Кредитная карта #картакарта» по действующим условиям</p>
+                                <h4>{t("bannersFirst.text-main-3")}</h4>
+                                <p>{t("bannersFirst.text-desc-3")}</p>
 
-                                <h4>Условия</h4>
-                                <p>Приз: 1 000 000 тенге на каждого победителя. В рамках Акции -   19 победителей в каждом филиале.</p>
-                                <h4>Условия Акции:</h4>
-                                <p>1. Оформить #картакарта с 01.03.2020 г. по 31.03.2020 г. и с даты получения карты в течение 3-х календарных дней провести по карте покупку(-и) в POS/Internet на общую сумму не менее 10 000 тенге.</p>
-                                <p>2. Определение победителей 07.04.2020 г.</p>
-                                <h4>Территория проведения Акции: Филиальная сеть Банка ЦентрКредит Правила Акции для клиентов Банка:</h4>
-                                <p>1. Клиенты Банка, оформившие #картакарта в период Акции, и совершившие в течение 3-х календарных дней с даты получения карты покупку(-и) в POS/Internet на общую сумму не менее 10 000 тенге, становятся участниками Акции.</p>
-                                <p>2. В случае отмены покупки клиентом в период Акции и подведения итогов, такая покупка не засчитывается за выполнение условия Акции. 3. На момент определения победителей #картакарта должна быть действующей (незакрытой).</p>
-                                <h4>Не признаются участниками Акции и не имеют права принимать в ней участие:</h4>
-                                <p>1. Нерезиденты РК</p>
-                                <p>2. Сотрудники и аффилированные лица Банка ЦентрКредит</p>
-                                <h4>Как определяются победители:</h4>
-                                <p>Победитель будет выбран по номеру договора генератором случайных чисел.</p>
-                                <p>Розыгрыш будет проведен в режиме онлайн-трансляции в социальных сетях Инстаграм и Фейсбук.</p>
-                                <h4>Особые условия:</h4>
-                                <p>1. Организаторы Акции вправе переиграть процесс определения победителя, если выявленный победитель не выйдет на связь с менеджерами Банка в течение 24 часов после окончания конкурса.</p>
-                                <p>2. Денежный выигрыш зачисляется на депозит клиента в Банке ЦентрКредит (действующий с возможностью пополнения/новый).</p>
-                                <p>3. В течение периода проведения Акции Организатор вправе вносить изменения в настоящие Правила в одностороннем порядке; в случае внесения изменений и/или дополнений в настоящие Правила распространяет информацию о внесенных изменениях на сайте Банка <a target="_blank" href="https://www.bcc.kz/">www.bcc.kz</a></p>
-                                <h4>Призовой фонд (общий и в разрезе филиалов) </h4>
-                                <p>1. 19 000 000 (девятнадцать миллионов) тенге.</p>
-                                <p>2. 1 000 000 (один миллион) тенге на каждый филиал.</p>
-                                <h4>Периодичность розыгрыша/подвед ения итогов</h4>
-                                <p>Подведение итогов - 07.04.2020 г.</p>
-                                <p>Определение победителя в онлайн трансляции (Инстаграм и Фейсбук) с Алматы (ГО), по номеру договора.</p>
+                                <h4>{t("bannersFirst.text-main-4")}</h4>
+                                <p>{t("bannersFirst.text-desc-4")}</p>
+
+                                <h4>{t("bannersFirst.text-main-5")}</h4>
+                                <p>{t("bannersFirst.text-desc-5")}</p>
+                                <p>{t("bannersFirst.text-desc-6")}</p>
+
+                                <h4>{t("bannersFirst.text-main-123")}</h4>
+                                <p>{t("bannersFirst.text-desc-123")}</p>
+                                <h4>{t("bannersFirst.text-main-6")}</h4>
+                                <p>{t("bannersFirst.text-desc-7")}</p>
+                                <p>{t("bannersFirst.text-desc-8")}</p>
+                                <p>{t("bannersFirst.text-desc-81")}</p>
+
+                                <h4>{t("bannersFirst.text-main-7")}</h4>
+                                <p>{t("bannersFirst.text-desc-9")}</p>
+                                <p>{t("bannersFirst.text-desc-10")}</p>
+
+                                <h4>{t("bannersFirst.text-main-8")}</h4>
+                                <p>{t("bannersFirst.text-desc-11")}</p>
+                                <p>{t("bannersFirst.text-desc-12")}</p>
+
+                                <h4>{t("bannersFirst.text-main-9")}</h4>
+                                <p>{t("bannersFirst.text-desc-13")}</p>
+                                <p>{t("bannersFirst.text-desc-14")}</p>
+                                <p>{t("bannersFirst.text-desc-15")} <a target="_blank" href="https://www.bcc.kz/">www.bcc.kz</a></p>
+
+                                <h4>{t("bannersFirst.text-main-10")}</h4>
+                                <p>{t("bannersFirst.text-desc-16")}</p>
+                                <p>{t("bannersFirst.text-desc-17")}</p>
+
+                                <h4>{t("bannersFirst.text-main-11")}</h4>
+                                <p>{t("bannersFirst.text-desc-18")}</p>
+                                <p>{t("bannersFirst.text-desc-19")}</p>
                             </Grid>
 
                         </Grid>
@@ -397,6 +423,33 @@ const Banner = () => {
 
 
                     <div className={classes.drpBack} onClick={() => setOpen2(false)}></div>
+                </Grid>
+
+                <Grid className={`${classes.fixedModal} ${open3 ? classes.open : ''}`}>
+
+
+                    <Grid className={classes.modalClose}>
+                        <IconButton className={classes.fixedBtnModal} onClick={() => setOpen3(false)}><CloseIcon /></IconButton>
+                        <Grid className={classes.modalMain}>
+                            <Grid className={classes.modalTitle}>
+                                <h3>{t("banners.text_0")}</h3>
+                                <p>{t("banners.text_1")}</p>
+                                <p>{t("banners.text_2")}</p>
+                                <p>{t("banners.text_3")}</p>
+                                <p>{t("banners.text_4")}</p>
+                                <p>{t("banners.text_5")}</p>
+                                <p>{t("banners.text_6")}</p>
+                                <p>{t("banners.text_7")}</p>
+                                <p>{t("banners.text_8")}</p>
+                                <p>{t("banners.text_9")}</p>
+                            </Grid>
+
+                        </Grid>
+
+                    </Grid>
+
+
+                    <div className={classes.drpBack} onClick={() => setOpen3(false)}></div>
                 </Grid>
 
 
