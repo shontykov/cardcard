fa65fed8ca1174a4d4201f7b65690f690de7dfaa
shontykov <shontykov.kz@gmail.com>
2020-05-13 16:09:45 +0600

    Updates

--- precache-manifest.3838b0a3df532af2172c386d56145487.js
+++ precache-manifest.3838b0a3df532af2172c386d56145487.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "992eb6bcdbc7e2b38581658178acc97c",
+    "revision": "addbf4ece1b4cb5df77d075810bc68b5",
     "url": "/cardcard/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cardcard/static/css/2.b650302e.chunk.css"
   },
   {
-    "revision": "b911eda9e5782591d6ec",
+    "revision": "250d5e2fabb4db258ee0",
     "url": "/cardcard/static/css/main.755b33c0.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cardcard/static/js/2.619dfee7.chunk.js.LICENSE"
   },
   {
-    "revision": "b911eda9e5782591d6ec",
-    "url": "/cardcard/static/js/main.df768054.chunk.js"
+    "revision": "250d5e2fabb4db258ee0",
+    "url": "/cardcard/static/js/main.e6d2754c.chunk.js"
   },
   {
     "revision": "cb520f21e13a6a54699d",
